c7a44c2ff4e33623d7aa42254bf969c6585188ce
Kieu Oanh <kieuoanh@example.com>
2026-01-28 09:57:13 +0700

    kieuoanh

--- TEAM_00/kieuoanh/LED_Blink/src/main.cpp
+++ TEAM_00/kieuoanh/LED_Blink/src/main.cpp
@@ -1,13 +1,28 @@
 #include <Arduino.h>
 
+#define PIN_LED_RED 23
+
+// Hàm kiểm tra thời gian (Non-blocking)
+bool IsReady(unsigned long &ulTimer, uint32_t millisecond) {
+  if (millis() - ulTimer < millisecond) return false;
+  ulTimer = millis();
+  return true;
+}
+
 void setup() {
-  // Chân 13 thường là chân đèn LED có sẵn trên mạch Uno
-  pinMode(13, OUTPUT);
+  Serial.begin(115200); // Khởi động Serial Monitor
+  printf("WELCOME IOT\n");
+  pinMode(PIN_LED_RED, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(13, HIGH); // Bật đèn
-  delay(1000);            // Đợi 1 giây
-  digitalWrite(13, LOW);  // Tắt đèn
-  delay(1000);            // Đợi 1 giây
+  static unsigned long ulTimer = 0;
+  static bool ledStatus = false;
+
+  // Cứ mỗi 500ms thì đổi trạng thái đèn 1 lần
+  if (IsReady(ulTimer, 500)){
+    ledStatus = !ledStatus;
+    printf("LED IS %s\n", ledStatus ? "ON" : "OFF");
+    digitalWrite(PIN_LED_RED, ledStatus ? HIGH : LOW);
+  }
 }
